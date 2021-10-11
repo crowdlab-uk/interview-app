810d1e32104f5b21421b233efe52c28013d583ce
kritikaarorapandhi <92295693+kritikaarorapandhi@users.noreply.github.com>
2021-10-11 14:24:04 +0530

    conditional rendering of AddPost component

--- src/Posts.js
+++ src/Posts.js
@@ -1,6 +1,9 @@
 // @flow
 import React from 'react';
 import Post from './Post';
+import AddPost from './AddPost';
+import { useState } from 'react';
+
 
 type PostType = {
   id: number,
@@ -12,13 +15,23 @@ type Props = {
   posts: Array<PostType>,
 };
 
+
 function Posts({ posts }: Props) {
+  const [showForm, setFormState] = useState(false);
+
+  const onAddPostClick = () => {
+    setFormState(true);
+  }
   return (
     <div>
       {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
-      <button onClick={() => alert('TODO: make this work')}>Add Post</button>
+      <button onClick={() => {onAddPostClick()}}>Add Post</button>
+      {
+        showForm ? <AddPost></AddPost> : ''
+      }
+      
     </div>
   );
 }
